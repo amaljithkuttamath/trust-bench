275444e71dde8e12e784d694e8f3c88debc84cd7
Amaljith Kuttamath <kuttamath.amaljith@gmail.com>
2026-03-12 08:50:56 -0400

    feat: register hallucination and cross-lingual probes

--- trust_bench/probes/registry.py
+++ trust_bench/probes/registry.py
@@ -2,10 +2,14 @@
 
 from trust_bench.models.base import ConfigError
 from trust_bench.probes.base import Probe
+from trust_bench.probes.cross_lingual import CrossLingualProbe
 from trust_bench.probes.feature_survey import FeatureSurveyProbe
+from trust_bench.probes.hallucination import HallucinationProbe
 
 PROBE_REGISTRY: dict[str, type[Probe]] = {
+    "cross_lingual": CrossLingualProbe,
     "feature_survey": FeatureSurveyProbe,
+    "hallucination": HallucinationProbe,
 }
 
 
